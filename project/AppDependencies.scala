093927107bb638c791b4c78f5eba993b46493bcc
abiodxn <bolaji.abiodun94@gmail.com>
2022-03-24 23:30:15 +0000

    SASS-2301 Implemented PensionProviderPaid

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.21.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.20.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.7.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.62.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.21.0"             % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.20.0"             % Test,
     "org.scalatest"           %% "scalatest"                  % "3.2.9"              % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.62.0"             % Test,
     "org.jsoup"               %  "jsoup"                      % "1.13.1"             % Test,
