439fe6adc8cbb2ac90b5bf614b3e6956167f1ef7
abiodxn <bolaji.abiodun94@gmail.com>
2022-12-14 10:13:00 +0000

    SASS 2957 - Add pension reference summary page for unauth pensions

---
+++ app/controllers/pensions/unauthorisedPayments/UkPensionSchemeDetailsController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.pensions.unauthorisedPayments
+
+import config.AppConfig
+import controllers.pensions.incomeFromPensions.routes.UkPensionIncomeCYAController
+import controllers.predicates.AuthorisedAction
+import models.pension.statebenefits.UkPensionIncomeViewModel
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.PensionSessionService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.pensions.incomeFromPensions.UkPensionIncomeSummary
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import controllers.pensions.routes.PensionsSummaryController
+import controllers.predicates.TaxYearAction.taxYearAction
+import views.html.pensions.unauthorisedPayments.UkPensionSchemeDetails
+
+class UkPensionSchemeDetailsController @Inject()(implicit val cc: MessagesControllerComponents,
+                                                 authAction: AuthorisedAction,
+                                                 ukPensionSchemeDetails: UkPensionSchemeDetails,
+                                                 appConfig: AppConfig,
+                                                 pensionSessionService: PensionSessionService)
+  extends FrontendController(cc) with I18nSupport {
+
+  def show(taxYear: Int): Action[AnyContent] = (authAction andThen taxYearAction(taxYear)).async { implicit request =>
+    pensionSessionService.getPensionsSessionDataResult(taxYear, request.user) {
+      case Some(data) =>
+        val pensionSchemeTaxReferenceList: Seq[String] = data.pensions.unauthorisedPayments.pensionSchemeTaxReference.getOrElse(Seq.empty)
+        Future(Ok(ukPensionSchemeDetails(taxYear, pensionSchemeTaxReferenceList)))
+      case None =>
+        Future.successful(Redirect(controllers.pensions.routes.PensionsSummaryController.show(taxYear)))
+    }
+  }
+}
