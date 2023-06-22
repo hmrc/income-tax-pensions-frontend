8b14d357060f06eeb32d951c2e3f9c4764be8a6a
Hassan Shahab <4068568+hs3t@users.noreply.github.com>
2023-06-22 06:29:34 +0100

    SASS-3549: Updated test after merging main

--- it/utils/PageUrls.scala
+++ it/utils/PageUrls.scala
@@ -159,7 +159,7 @@ object PageUrls extends IntegrationTest {
     def pensionTaxReferenceNumberLifetimeAllowanceUrl(taxYear: Int): String = s"$appUrl/$taxYear/annual-lifetime-allowances/pension-scheme-tax-reference"
 
     def pensionTaxReferenceNumberLifetimeAllowanceUrlWithIndex(taxYear: Int, index: Int): String =
-      s"$appUrl/$taxYear/lifetime-allowance/pension-scheme-tax-reference?pensionSchemeTaxReferenceIndex=$index"
+      s"$appUrl/$taxYear/annual-lifetime-allowances/pension-scheme-tax-reference?pensionSchemeTaxReferenceIndex=$index"
 
     def pensionTaxReferenceNumberLifetimeAllowanceUrlIndex: Int => Int => String = (pensionSchemeTaxReference: Int) =>
       (taxYear: Int) => s"$appUrl/$taxYear/annual-lifetime-allowances/pension-scheme-tax-reference?pensionSchemeTaxReferenceIndex=$pensionSchemeTaxReference"
