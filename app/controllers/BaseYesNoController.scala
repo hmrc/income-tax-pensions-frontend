f6cb14749c4609ce6ab03c8f7a5e5729632c71ae
harry-ritchie <111759395+harry-ritchie@users.noreply.github.com>
2024-01-26 13:28:57 +0000

    Fix some warnings regarding using a global execution context (#347)
    * Fix some warnings regarding using a global execution context
    
    * Fix build
    
    * Remove redundant config

--- app/controllers/BaseYesNoController.scala
+++ app/controllers/BaseYesNoController.scala
@@ -30,20 +30,17 @@ import services.PensionSessionService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import utils.Clock
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
+abstract class BaseYesNoController @Inject() (messagesControllerComponents: MessagesControllerComponents,
+                                              pensionSessionService: PensionSessionService,
+                                              authAction: AuthorisedAction,
+                                              errorHandler: ErrorHandler)(implicit appConfig: AppConfig, clock: Clock, ec: ExecutionContext)
+    extends FrontendController(messagesControllerComponents) {
 
-abstract class BaseYesNoController(
-                                    messagesControllerComponents: MessagesControllerComponents,
-                                    pensionSessionService: PensionSessionService,
-                                    authAction: AuthorisedAction,
-                                    errorHandler: ErrorHandler)
-                                  (implicit appConfig: AppConfig, clock: Clock)
-  extends FrontendController(messagesControllerComponents) {
-
-  def prepareView(pensionsUserData: PensionsUserData, taxYear: Int)
-                 (implicit request: AuthorisationRequest[AnyContent]): Html
+  def prepareView(pensionsUserData: PensionsUserData, taxYear: Int)(implicit request: AuthorisationRequest[AnyContent]): Html
 
   def redirectWhenNoSessionData(taxYear: Int): Result
 
@@ -53,8 +50,7 @@ abstract class BaseYesNoController(
 
   def proposedUpdatedSessionDataModel(currentSessionData: PensionsUserData, yesSelected: Boolean): PensionsCYAModel
 
-  def whenFormIsInvalid(form: Form[Boolean], taxYear: Int)
-                       (implicit request: AuthorisationRequest[AnyContent]): Html
+  def whenFormIsInvalid(form: Form[Boolean], taxYear: Int)(implicit request: AuthorisationRequest[AnyContent]): Html
 
   def errorMessageSet: YesNoForm
 
@@ -72,7 +68,7 @@ abstract class BaseYesNoController(
     }
   }
 
-  def submit(taxYear: Int): Action[AnyContent] = authAction.async { implicit request => {
+  def submit(taxYear: Int): Action[AnyContent] = authAction.async { implicit request =>
     pensionSessionService.getPensionSessionData(taxYear, request.user).flatMap {
       case Left(_) => Future.successful(onError)
       case Right(pensionsUserDataOpt) =>
@@ -81,29 +77,24 @@ abstract class BaseYesNoController(
           .getOrElse(Future.successful(redirectWhenNoSessionData(taxYear)))
     }
   }
-  }
 
-  protected def populateForm(pensionsUserData: PensionsUserData)
-                            (implicit request: AuthorisationRequest[AnyContent]): Form[Boolean] = {
+  protected def populateForm(pensionsUserData: PensionsUserData)(implicit request: AuthorisationRequest[AnyContent]): Form[Boolean] = {
     val baseForm = form(request.user.isAgent)
     questionOpt(pensionsUserData) match {
-      case Some(true) => baseForm.fill(true)
+      case Some(true)  => baseForm.fill(true)
       case Some(false) => baseForm.fill(false)
-      case None => baseForm
+      case None        => baseForm
     }
   }
 
   protected def redirectToSummaryPage(taxYear: Int): Result = Redirect(controllers.pensions.routes.PensionsSummaryController.show(taxYear))
 
-  private def show(pensionsUserData: PensionsUserData, taxYear: Int)
-                  (implicit request: AuthorisationRequest[AnyContent]): Future[Result] =
+  private def show(pensionsUserData: PensionsUserData, taxYear: Int)(implicit request: AuthorisationRequest[AnyContent]): Future[Result] =
     Future.successful(Ok(prepareView(pensionsUserData, taxYear)))
 
-
-  private def ensureThatSessionDataIsSufficient(pensionsUserData: PensionsUserData, taxYear: Int)
-                                               (f: (PensionsUserData, Int) => Future[Result]): Future[Result] =
+  private def ensureThatSessionDataIsSufficient(pensionsUserData: PensionsUserData, taxYear: Int)(
+      f: (PensionsUserData, Int) => Future[Result]): Future[Result] =
     if (sessionDataIsSufficient(pensionsUserData)) f(pensionsUserData, taxYear) else Future.successful(whenSessionDataIsInsufficient(taxYear))
-
 
   private def onError(implicit request: AuthorisationRequest[AnyContent]): Result = errorHandler.handleError(INTERNAL_SERVER_ERROR)
 
@@ -111,17 +102,18 @@ abstract class BaseYesNoController(
     missingInputError = errorMessageSet.noEntry.get(isAgent)
   )
 
+  private def submit(pensionsUserData: PensionsUserData, taxYear: Int)(implicit request: AuthorisationRequest[AnyContent]): Future[Result] =
+    form(request.user.isAgent)
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(whenFormIsInvalid(formWithErrors, taxYear))),
+        validForm => onValidForm(pensionsUserData, taxYear, validForm))
 
-  private def submit(pensionsUserData: PensionsUserData, taxYear: Int)
-                    (implicit request: AuthorisationRequest[AnyContent]): Future[Result] =
-    form(request.user.isAgent).bindFromRequest().fold(
-      formWithErrors => Future.successful(BadRequest(whenFormIsInvalid(formWithErrors, taxYear))),
-      validForm => onValidForm(pensionsUserData, taxYear, validForm))
-
-  private def onValidForm(pensionsUserData: PensionsUserData, taxYear: Int, validForm: Boolean)
-                         (implicit request: AuthorisationRequest[AnyContent], clock: Clock): Future[Result] =
+  private def onValidForm(pensionsUserData: PensionsUserData, taxYear: Int, validForm: Boolean)(implicit
+      request: AuthorisationRequest[AnyContent],
+      clock: Clock): Future[Result] =
     validForm match {
-      case (yesWasSelected) =>
+      case yesWasSelected =>
         pensionSessionService.createOrUpdateSessionData(
           request.user,
           proposedUpdatedSessionDataModel(pensionsUserData, yesWasSelected),
--- app/controllers/pensions/unauthorisedPayments/UkPensionSchemeDetailsController.scala
+++ app/controllers/pensions/unauthorisedPayments/UkPensionSchemeDetailsController.scala
@@ -30,17 +30,19 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.pensions.unauthorisedPayments.UkPensionSchemeDetails
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class UkPensionSchemeDetailsController @Inject()(implicit val cc: MessagesControllerComponents,
-                                                 authAction: AuthorisedAction,
-                                                 ukPensionSchemeDetails: UkPensionSchemeDetails,
-                                                 appConfig: AppConfig,
-                                                 pensionSessionService: PensionSessionService,
-                                                 errorHandler: ErrorHandler)
-  extends FrontendController(cc) with I18nSupport {
+class UkPensionSchemeDetailsController @Inject() (implicit
+    val cc: MessagesControllerComponents,
+    authAction: AuthorisedAction,
+    ukPensionSchemeDetails: UkPensionSchemeDetails,
+    appConfig: AppConfig,
+    pensionSessionService: PensionSessionService,
+    errorHandler: ErrorHandler,
+    ec: ExecutionContext)
+    extends FrontendController(cc)
+    with I18nSupport {
 
   def show(taxYear: Int): Action[AnyContent] = (authAction andThen taxYearAction(taxYear)).async { implicit request =>
     pensionSessionService.getPensionSessionData(taxYear, request.user).flatMap {
@@ -48,7 +50,6 @@ class UkPensionSchemeDetailsController @Inject()(implicit val cc: MessagesContro
       case Right(optData) =>
         val checkRedirect = journeyCheck(PSTRSummaryPage, _: PensionsCYAModel, taxYear)
         redirectBasedOnCurrentAnswers(taxYear, optData, cyaPageCall(taxYear))(checkRedirect) { data =>
-
           val pensionSchemeTaxReferenceList: Seq[String] = data.pensions.unauthorisedPayments.pensionSchemeTaxReference.getOrElse(Seq.empty)
           Future(Ok(ukPensionSchemeDetails(taxYear, pensionSchemeTaxReferenceList)))
         }
