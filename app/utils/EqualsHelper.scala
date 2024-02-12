2ff183e11882c324ffeac3e5e87c0ee238ce7cbf
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2024-02-12 14:42:47 +0000

    commit

--- app/utils/EqualsHelper.scala
+++ app/utils/EqualsHelper.scala
@@ -18,8 +18,7 @@ package utils
 
 object EqualsHelper {
 
-  def isDifferent[A](left: A, right: Option[A]): Boolean = {
+  def isDifferent[A](left: A, right: Option[A]): Boolean =
     Option(left) != right
-  }
 
 }
