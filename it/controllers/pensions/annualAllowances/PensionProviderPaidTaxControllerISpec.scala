db7152d077213dc721c204f7b677a50604fd4cf9
Hassan Shahab <4068568+hs3t@users.noreply.github.com>
2024-03-15 12:31:27 +0000

    SASS-7414: Organise Imports

--- it/controllers/pensions/annualAllowances/PensionProviderPaidTaxControllerISpec.scala
+++ it/controllers/pensions/annualAllowances/PensionProviderPaidTaxControllerISpec.scala
@@ -29,7 +29,7 @@ import play.api.http.HeaderNames
 import play.api.http.Status.{OK, SEE_OTHER}
 import play.api.libs.ws.WSResponse
 import utils.PageUrls.PensionAnnualAllowancePages._
-import utils.PageUrls.{fullUrl, pensionSummaryUrl}
+import utils.PageUrls.fullUrl
 import utils.{IntegrationTest, PensionsDatabaseHelper, ViewHelpers}
 
 class PensionProviderPaidTaxControllerISpec extends IntegrationTest with ViewHelpers with PensionsDatabaseHelper {
