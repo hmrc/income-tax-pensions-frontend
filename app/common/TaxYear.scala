6a93b9495ed3a4ecf4af305cca4f5f5f8dd5ff0a
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2024-04-26 13:44:55 +0100

    tidy up

--- app/common/TaxYear.scala
+++ app/common/TaxYear.scala
@@ -18,7 +18,6 @@ package common
 
 import play.api.mvc.PathBindable
 
-
 /** Represents a tax year in the format yyyy - yyyy+1, e.g. 2023-2024. We keep only the last part (2024) which is the end of the tax year.
   */
 final case class TaxYear(endYear: Int) extends AnyVal {
