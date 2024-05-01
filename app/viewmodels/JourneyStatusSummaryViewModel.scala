f40e46b47554338d7efe9afbbbb444d48cf26723
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2024-05-01 14:51:35 +0100

    clean up

--- app/viewmodels/JourneyStatusSummaryViewModel.scala
+++ app/viewmodels/JourneyStatusSummaryViewModel.scala
@@ -110,9 +110,10 @@ object JourneyStatusSummaryViewModel {
       if (status == Completed) messages(s"common.status.$status")
       else s"""<strong class="govuk-tag govuk-tag--blue">${messages(s"common.status.$status")}</strong>"""
 
+    val journeyId = "${journey.toString}-link"
     s"""
        |  <span class="app-task-list__task-name">
-       |    <a id="${journey.toString}-link" class="govuk-link govuk-task-list__link" href=$href aria-describedby=$id> ${messages(s"journey.${journey.toString}")} </a>
+       |    <a id="$journeyId class="govuk-link govuk-task-list__link" href=$href aria-describedby=$id> ${messages(s"journey.${journey.toString}")} </a>
        |  </span>
        |  <div class="right-float" id=$id> $statusTag </div>
        |""".stripMargin
