14da0c0e9719d70274fbb3dcc56cec976c64fb66
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-08-13 15:30:27 +0100

    MIPR-1948: Upgrade Submissions dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,8 +18,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "10.1.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -28,7 +28,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"                  % "2.19.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"                  % "2.19.2",
     "org.codehaus.janino"           % "janino"                                % "3.1.12" // it's required by logback for conditional logging
   )
 
@@ -38,11 +38,11 @@ object AppDependencies {
     "org.scalacheck"         %% "scalacheck"              % "1.18.1",
     "org.scalatestplus"      %% "scalacheck-1-18"         % "3.2.19.0",
     "org.mockito"            %% "mockito-scala-scalatest" % "2.0.0",
-    "org.jsoup"               % "jsoup"                   % "1.20.1",
+    "org.jsoup"               % "jsoup"                   % "1.21.1",
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8",
-    "org.playframework"      %% "play-test"               % "3.0.7",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1",
-    "org.scalamock"          %% "scalamock"               % "7.3.2"
+    "org.playframework"      %% "play-test"               % "3.0.8",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2",
+    "org.scalamock"          %% "scalamock"               % "7.4.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
