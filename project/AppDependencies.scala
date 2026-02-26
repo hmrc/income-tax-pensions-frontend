014aa9452078756cf993553d5e52683589730e93
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-02-26 17:34:42 +0000

    dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,17 +18,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.31.0",
-    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                             % "2.13.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"                  % "2.21.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"                  % "2.21.1",
     "org.codehaus.janino"           % "janino"                                % "3.1.12" // it's required by logback for conditional logging
   )
 
